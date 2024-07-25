eae296c060b1abd6cbdda00b606d923c80ee3c84
Niv Ozer <niv.ozer@yahoo.com>
2024-07-25 19:58:43 +0300

    Renaming

--- app/src/main/java/com/example/brainy/ui/brainy/BrainyFragment.kt
+++ app/src/main/java/com/example/brainy/ui/brainy/BrainyFragment.kt
@@ -1,4 +1,4 @@
-package com.example.brainy.ui.dashboard
+package com.example.brainy.ui.brainy
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.brainy.databinding.FragmentDashboardBinding
+import com.example.brainy.databinding.FragmentBrainyBinding
 
-class DashboardFragment : Fragment() {
+class BrainyFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentBrainyBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val brainyViewModel =
+            ViewModelProvider(this).get(BrainyViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentBrainyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        brainyViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/brainy/ui/brainy/BrainyViewModel.kt
+++ app/src/main/java/com/example/brainy/ui/brainy/BrainyViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.brainy.ui.dashboard
+package com.example.brainy.ui.brainy
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class BrainyViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
