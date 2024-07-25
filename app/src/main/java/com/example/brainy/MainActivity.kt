a545390f0c03090c3a11975faedab6c8f1d9380e
Niv Ozer <niv.ozer@yahoo.com>
2024-07-25 19:39:27 +0300

    Actionbar removed + enabled edge to edge

--- app/src/main/java/com/example/brainy/MainActivity.kt
+++ app/src/main/java/com/example/brainy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.brainy
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -31,5 +32,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        enableEdgeToEdge()
+        supportActionBar?.hide()
+
     }
 }
