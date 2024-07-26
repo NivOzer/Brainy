c724fd96edb0ae9865012b64718250be2db1552c
Niv Ozer <niv.ozer@yahoo.com>
2024-07-26 14:42:10 +0300

    Brainy Question Works

--- app/src/main/java/com/example/brainy/ui/shared/SharedViewModel.kt
+++ app/src/main/java/com/example/brainy/ui/shared/SharedViewModel.kt
@@ -8,6 +8,11 @@ class SharedViewModel : ViewModel() {
     private val _difficulty = MutableLiveData<Int>()
     val difficulty: LiveData<Int> get() = _difficulty
 
+    init {
+        // Set default value to a valid difficulty level
+        _difficulty.value = 0
+    }
+
     fun setDifficulty(level: Int) {
         _difficulty.value = level
     }
