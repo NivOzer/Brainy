f528f61086bca765622a7fa161d9ddbc1473482e
Niv Ozer <niv.ozer@yahoo.com>
2024-07-27 13:34:16 +0300

    Night mode button implemented

--- app/src/main/java/com/example/brainy/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/brainy/ui/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.brainy.databinding.FragmentSettingsBinding
@@ -28,6 +29,15 @@ class SettingsFragment : Fragment() {
 
         setupDifficultySlider()
         setupTimeDifficultySlider()
+
+        binding.nightModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
         return root
     }
 
